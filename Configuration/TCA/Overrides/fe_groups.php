e959fe72d14d80c46ce7d3d068c7f72ad58969cc
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2015-04-08 22:45:19 +0200

    [TASK] Use new structure for TCA and TCA overrides
    Since TYPO3 6.2.1 TCA overrides should be in Configuration/TCA/Overrides
    and the whole TCA configuration for a table can be in Configuration/TCA
    while the file name is identical with the table.

---
+++ Configuration/TCA/Overrides/fe_groups.php
@@ -0,0 +1,60 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2015 AOE GmbH <dev@aoe.com>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+defined('TYPO3_MODE') or die();
+
+// Add IP Options to fe_groups table
+$tempColumns = array(
+	'tx_aoeipauth_ip' => array(
+		'label' => 'LLL:EXT:aoe_ipauth/Resources/Private/Language/locallang_db.xml:fe_groups.tx_aoeipauth_ip',
+		'exclude' => 1,
+		'config' => Array (
+			'type' => 'inline',
+			'foreign_table' => 'tx_aoeipauth_domain_model_ip',
+			'foreign_field' => 'fe_group',
+			'maxitems' => 9999,
+			'appearance' => array(
+				'collapse' => 1,
+				'levelLinksPosition' => 'top',
+				'showSynchronizationLink' => 0,
+				'showPossibleLocalizationRecords' => 0,
+				'showAllLocalizationLink' => 0
+			),
+		),
+	),
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+	'fe_groups',
+	$tempColumns,
+	1
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+	'fe_groups',
+	'tx_aoeipauth_ip;;;;1-1-1',
+	'',
+	'after:lockToDomain'
+);
+
+unset($tempColumns);
--- ext_tables.php
+++ ext_tables.php
@@ -10,100 +10,15 @@ if (!defined('TYPO3_MODE')) {
 	'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_tx_aoeipauth_domain_model_ip.xml'
 );
 
-$TCA['tx_aoeipauth_domain_model_ip'] = array(
-	'ctrl' => array(
-		'title'	=> 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_db.xml:tx_aoeipauth_domain_model_ip',
-		'label' => 'description',
-		'tstamp' => 'tstamp',
-		'crdate' => 'crdate',
-		'cruser_id' => 'cruser_id',
-		'dividers2tabs' => TRUE,
-		'delete' => 'deleted',
-		'versioning' => FALSE,
-		'versioningWS' => FALSE,
-		'enablecolumns' => array(
-			'disabled' => 'hidden',
-			'starttime' => 'starttime',
-			'endtime' => 'endtime',
-		),
-		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Ip.php',
-		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_aoeipauth_domain_model_ip.png'
-	),
-);
-
-// Add IP Options to fe_users table
-$tempColumns = array(
-	'tx_aoeipauth_ip' => array(
-		'label' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_db.xml:fe_users.tx_aoeipauth_ip',
-		'exclude' => 1,
-		'config' => Array (
-			'type' => 'inline',
-			'foreign_table' => 'tx_aoeipauth_domain_model_ip',
-			'foreign_field' => 'fe_user',
-			'maxitems' => 9999,
-			'appearance' => array(
-				'collapse' => 1,
-				'levelLinksPosition' => 'top',
-				'showSynchronizationLink' => 0,
-				'showPossibleLocalizationRecords' => 0,
-				'showAllLocalizationLink' => 0
-			),
-		),
-	),
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
-	'fe_users',
-	$tempColumns,
-	1
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
-	'fe_users',
-	'tx_aoeipauth_ip;;;;1-1-1',
-	'',
-	'after:lockToDomain'
-);
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
 	'fe_users',
 	'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_fe_users.xml'
 );
-unset($tempColumns);
 
-// Add IP Options to fe_groups table
-$tempColumns = array(
-	'tx_aoeipauth_ip' => array(
-		'label' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_db.xml:fe_groups.tx_aoeipauth_ip',
-		'exclude' => 1,
-		'config' => Array (
-			'type' => 'inline',
-			'foreign_table' => 'tx_aoeipauth_domain_model_ip',
-			'foreign_field' => 'fe_group',
-			'maxitems' => 9999,
-			'appearance' => array(
-				'collapse' => 1,
-				'levelLinksPosition' => 'top',
-				'showSynchronizationLink' => 0,
-				'showPossibleLocalizationRecords' => 0,
-				'showAllLocalizationLink' => 0
-			),
-		),
-	),
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
-	'fe_groups',
-	$tempColumns,
-	1
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
-	'fe_groups',
-	'tx_aoeipauth_ip;;;;1-1-1',
-	'',
-	'after:lockToDomain'
-);
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
 	'fe_groups',
 	'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_fe_groups.xml'
 );
-unset($tempColumns);
 
 if ('BE' === TYPO3_MODE) {
 	// registering reports
