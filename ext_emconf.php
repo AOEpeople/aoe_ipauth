06ad47b4dc77d71887a68de10e7d36d96195dc22
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2017-10-05 19:47:56 +0200

    [TASK] Fixing Unit+Functional Tests

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.0.2',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.1-7.99.99',
+            'typo3' => '6.2.1-7.6.99',
         ),
         'conflicts' => array(
         ),
