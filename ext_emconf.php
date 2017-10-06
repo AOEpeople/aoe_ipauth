e9ecc2fd11bbd1f9fcc371a4faa145471a086ad6
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2017-10-06 20:01:09 +0200

    [TASK] Added TYPO3 Version 8 to version constains

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.0.2',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.1-7.99.99',
+            'typo3' => '6.2.1-8.9.99'
         ),
         'conflicts' => array(
         ),
